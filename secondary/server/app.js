109193418089320ddff344343443d0d6c6c3d400
romanberezhok <rberezhok@tripadvisor.com>
2023-09-21 18:46:47 +0300

    feat: dockerize app

--- secondary/server/app.js
+++ secondary/server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const messagesRouter = require('./routes/messages');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const host = process.env.HOST ||'0.0.0.0';
 
 app.use(express.json());
