929647b61124a0762616f422f3b22d6a2da5183d
romanberezhok <rberezhok@tripadvisor.com>
2023-10-18 20:35:59 +0300

    fix: fix secondary nodes calls

--- server/helpers/common.js
+++ server/helpers/common.js
@@ -4,7 +4,8 @@ const getSecondaryNodesURLs = async () => {
         const { data } = await axios.get('http://service-discovery:8080/api/rawdata');
         return data['services']['secondary-server-replicated-log@docker']['loadBalancer']['servers'].map((obj) => obj['url']);
     } catch (error) {
-        throw new Error('Failed to get URLs of secondary nodes.')
+        console.warn('Failed to get URLs of secondary nodes.')
+        return [];
     }
 }
 
